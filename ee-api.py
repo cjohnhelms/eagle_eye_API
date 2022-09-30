5968f9ffa881836f8dc8195d57ae82033af35baf
Christopher Helms <cjohnhelms@gmail.com>
2022-09-29 20:25:40 -0500

    Authentication and Pulling Device Info
    Was able to get authenticated and started pulling device info.
    Implemented cookie management for the auth token. Realizing the API
    documentation is not great. Also added a menu for choosing from defined
    functions. Next goal is implement downloading footage.

--- ee-api.py
+++ ee-api.py
@@ -1,16 +1,73 @@
 import requests
-from requests.auth import HTTPBasicAuth
-import os
+import json
 
-uname = str(input('Please provide a username: '))
-pword = str(input('Please provide a password: '))
-akey = str(input('Please provide an API key: '))
-url = str(input('Please provide a URL: '))
+uname = 'cjohnhelms@gmail.com'                        #str(input('Please provide a username: '))
+pword = 'bry25kur64Q.4323'                            #str(input('Please provide a password: '))
+akey = 'ddf9bfba-4006-11ed-b8a3-5e143c58c6f2'         #str(input('Please provide an API key: '))
+subd = 'c013'                                         #str(input('Please provide the sub domain: '))
 
-my_headers = {'content-type':'application/json', 'Authentication':akey}
-my_data = {'username':uname, 'password':pword}
+s = requests.Session()
+my_headers = {"Content-Type": "application/json", "Authentication": akey}
+my_data = json.dumps({"username":uname, "password":pword})
 
 # Authentication
-response = requests.post(url, headers=my_headers, data=my_data)
+auth_token = s.post(url='https://login.eagleeyenetworks.com/g/aaa/authenticate', headers=my_headers, data=my_data)
+print(auth_token)
 
-print(response)
+# Authorization
+login = s.post('https://login.eagleeyenetworks.com/g/aaa/authorize', headers=my_headers, data=auth_token)
+print(login)
+
+######################################################################################################################################
+
+# Get List of Bridges
+def get_bridges():
+    bridgeList = s.get(url=f'http://{subd}.eagleeyenetworks.com/g/device/list', headers={"Authentication": akey})
+    print(bridgeList)
+    print(bridgeList.json())
+
+# Get Bridge
+def get_bridge():
+    pram = input('Please provide a valid ESN: ')
+    bridge = s.get(url=f'http://{subd}.eagleeyenetworks.com/g/device', headers={"Authentication": akey}, params={"id": pram})
+    print(bridge)
+    print(bridge.json())
+
+# Get List of Cameras
+def get_cameras():
+    cameraList = s.get(url=f'http://{subd}.eagleeyenetworks.com/g/device', headers={"Authentication": akey})
+    print(cameraList)
+    print(cameraList.json())
+
+# Get Camera
+def get_camera():
+    pram = input('Please provide a valid ESN: ')
+    camera = s.get(url=f'http://{subd}.eagleeyenetworks.com/g/device', headers={"Authentication": akey}, params={"id": pram})
+    print(camera)
+    print(camera.json())
+
+#######################################################################################################################################
+
+menu = {}
+menu['1']="Get List of Bridges" 
+menu['2']="Get Bridge"
+menu['3']="Get List of Cameras"
+menu['4']="Get Cammera"
+menu['5']="Exit"
+while True: 
+    options=menu.keys()
+    for entry in options: 
+        print(entry, menu[entry])
+    selection=input("Please Select: ") 
+    if selection =='1': 
+        get_bridges()
+    elif selection == '2': 
+        get_bridge()
+    elif selection == '3':
+        get_cameras()
+    elif selection == '4': 
+        get_camera()
+    elif selection == '5':
+        break
+    else: 
+      print("Unknown Option Selected.")
